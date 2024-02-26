16c3b25b23e428ccbe793ebe430ea7ab2f45c535
lenrick <lennart.rick@gmx.de>
2024-02-26 11:31:58 +0200

    added contact form and category

--- kontaktformular/resources/views/navigation-menu.blade.php
+++ kontaktformular/resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('contact.index') }}" :active="request()->routeIs('contact.index')">
+                        {{ __('ContactForm') }}
+                    </x-nav-link>
                 </div>
             </div>
 
